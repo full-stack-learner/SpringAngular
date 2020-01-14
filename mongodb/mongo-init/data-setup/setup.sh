9e44a329b00322904050039fb7fa276ecda24c00
Philipp Jahoda <phil.jahoda@gmail.com>
2020-01-14 15:37:25 +0100

    Fix issues with mongo-init script, fix issues with nginx

--- mongodb/mongo-init/data-setup/setup.sh
+++ mongodb/mongo-init/data-setup/setup.sh
@@ -1,8 +1,7 @@
 # The script does the following:
-# For each folder inside the data-import folder, it reads all json files and creates a database with the name of the folder.
+# For each folder inside the data-setup folder, it reads all folder names and creates a database with the name of the folder.
 # For each json file, it creates a collection with the name of the file and persists all json objects contained in the file.
-# Creates a new user used by the Spring Boot Server in production mode (username and password must match those in Spring application-production.yml).
-# 'rootuser' and 'rootpassword' are taken from docker-compose.yml (mongo).
+# This is a convenient way of "pre-filling" your database with data
 
 cd /data-setup
 pattern="./"
@@ -21,6 +20,8 @@ do
 done
 echo "Finished importing data"
 
+# Creates a new user used by the Spring Boot Server in production mode (username and password must match those in Spring application-production.yml).
+# 'rootuser' and 'rootpassword' as defined in docker-compose.yml, database name and users as defined in Spring MongoDB configuration
 echo "Creating user"
 mongo -host mongo -u rootuser -p rootpassword --authenticationDatabase admin --eval "db.createUser({user: 'productionuser', pwd: 'dsfaere56e4Uxo', roles: [{role: 'readWrite', db: 'database'}]});" database
 echo "Finished creating user"
