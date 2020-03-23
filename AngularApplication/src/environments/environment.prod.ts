030e4e6fc1e9d17b8760981e23ed215a0466debc
Philipp Jahoda <phil.jahoda@gmail.com>
2020-03-23 10:17:45 +0100

    Minor fixes and cleanup

--- AngularApplication/src/environments/environment.prod.ts
+++ AngularApplication/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // the IP address of the Spring server including port
-  apiUrl: 'http://46.101.159.55:8080',
+  apiUrl: 'http://your.domain.at:8443',
   // redirect URI back to the Angular application for oauth authorization code (no port needed because nginx defaults to 80 - see Dockerfile)
-  redirectUri: 'http://46.101.159.55'
+  redirectUri: 'http://your.domain.at'
 };
